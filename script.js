891bfa40fa5ccf66778c59bc4f4391c71e855f85
DebayanPal1 <24330814-DebayanPal1@users.noreply.replit.com>
2023-08-20 15:31:34 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let x=document.getElementById("a");
+
+  x.addEventListener("click",function(e){
+  alert("I");
+})
+
+let y=document.getElementById("b");
+  y.addEventListener("click",function(e){
+  alert("love");
+})
+
+let z=document.getElementById("c");
+  z.addEventListener("click",function(e){
+  alert("you");
+})
+
+
+
+
